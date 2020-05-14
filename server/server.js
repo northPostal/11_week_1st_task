7d0fab8f21370290fb67b626cab202a49167edbd
northPostal <north.postal@gmail.com>
2020-05-14 19:17:41 +0300

    upload task and add /server/test.json

--- server/server.js
+++ server/server.js
@@ -9,10 +9,34 @@ import sockjs from 'sockjs'
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
 
+const { readFile, writeFile, unlink } = require('fs').promises
+
+const setHeaders = (req, res, next) => {
+  res.set('x-skillcrucial-user', '46a02278-f502-4416-a401-707cfef5cc86')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  next()
+}
+
+const saveFile = async (users) => {
+  return writeFile(`${__dirname}/test.json`, JSON.stringify(users), { encoding: 'utf8' })
+}
+
+const fileRead = async () => {
+  return readFile(`${__dirname}/test.json`, { encoding: 'utf8' })
+    .then((data) => JSON.parse(data))
+    .catch(async () => {
+      const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+      await saveFile(users)
+      return users
+    })
+}
+
 let connections = []
 
 const port = process.env.PORT || 3000
 const server = express()
+
+server.use(setHeaders)
 
 server.use(cors())
 
@@ -23,15 +47,41 @@ server.use(bodyParser.json({ limit: '50mb', extended: true }))
 server.use(cookieParser())
 
 server.get('/api/v1/users/', async (req, res) => {
-  const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+  const users = await fileRead()
   res.json(users)
-})
+}) // read
 
-server.get('/api/v1/users/take/:number', async (req, res) => {
-  const { number } = req.params
-  const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
-  res.json(users.slice(0, +number))
-})
+server.post('/api/v1/users/', async (req, res) => {
+  const users = await fileRead()
+  const newUserBody = req.body
+  const userLength = users[users.length - 1].id
+  newUserBody.id = userLength + 1
+  const newUser = [...users, newUserBody]
+  saveFile(newUser)
+  res.json({ status: 'success', id: newUserBody.id })
+}) // read & write
+
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const users = await fileRead()
+  const { userId } = req.params
+  const newUserBody = req.body
+  const newUserArray = users.map((it) => (it.id === +userId ? Object.assign(it, newUserBody) : it))
+  saveFile(newUserArray)
+  res.json({ status: 'success', id: userId })
+}) // read & write
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const users = await fileRead()
+  const { userId } = req.params
+  users.splice(Number(userId) - 1, 1)
+  saveFile(users)
+  res.json({ status: 'success', id: Number(userId) })
+}) // read & write
+
+server.delete('/api/v1/users/', async (req, res) => {
+  unlink(`${__dirname}/test.json`)
+  res.json()
+}) // write
 
 server.use('/api/', (req, res) => {
   res.status(404)
@@ -78,3 +128,5 @@ echo.installHandlers(app, { prefix: '/ws' })
 
 // eslint-disable-next-line no-console
 console.log(`Serving at http://localhost:${port}`)
+
+// That's fucking awesome!
